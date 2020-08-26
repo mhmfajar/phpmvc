280e4c9e739dd34fb35bee30ed4b558b379a25b1
Muhammad Fajar <muhammadfajar191@gmail.com>
2020-08-26 14:22:44 +0700

    #6 Assets

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,3 +1,8 @@
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="<?= BASEURL; ?>/js/jquery/jquery-3.5.1.min.js"></script>
+<script src="<?= BASEURL; ?>/js/popper/popper-2.4.4.min.js"></script>
+<script src="<?= BASEURL; ?>/js/bootstrap/bootstrap.js"></script>
 </body>
 
 </html>
